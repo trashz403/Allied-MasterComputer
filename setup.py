14ce79869b1c6eb754b46609c90901c0656b25a6
TRASHZ403 <z403trash@proton.me>
2025-06-02 07:15:03 -0400

    setup.py update 2

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 
 def installation_processes():
     import subprocess
-    subprocess.run(["wget", "1.1.1.1"])
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    subprocess.run(["wget", "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q8_0.gguf"],cwd="models")
 
 
 installation_processes()
